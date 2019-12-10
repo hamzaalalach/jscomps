ec02d1a7840e2f0f5cf0713420e3be4110fff2d3
Hamza Alalach <hamzaalalach@gmail.com>
2019-12-11 00:02:30 +0100

    Make test functions use local package

--- test/jscomps.test.js
+++ test/jscomps.test.js
@@ -2,19 +2,19 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 test('Should output error if no watch folder provided.', () => {
-	return runJSComps('jscomps').then().catch((data) =>  {
+	return runJSComps('node jscomps').then().catch((data) =>  {
 		expect(data.stderr).not.toBe('');
 	});
 });
 
 test('Should output error if watch folder doesn\'t exist.', () => {
-	return runJSComps('jscomps -f something').then((data) => {
+	return runJSComps('node jscomps -f something').then((data) => {
 		expect(data.stdout).toBe('Not a valid watch folder.\n');
 	}).catch();
 });
 
 test('Should import files if watch folder is provided.', () => {
-	return runJSComps('jscomps -f example -w false').then((data) => {
+	return runJSComps('node jscomps -f example -w false').then((data) => {
 		expect(data.stdout).toBe('Files imported.\n');
 	}).catch();
 });
